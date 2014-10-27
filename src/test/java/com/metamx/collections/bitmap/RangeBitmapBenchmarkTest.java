fee6b448084b4c72bbd4a6f296aa8baa73bfd2da
Xavier Léauté <xavier@metamarkets.com>
2014-10-27 11:26:15 -0700

    separate benchmarks for different bitmap structures

---
+++ src/test/java/com/metamx/collections/bitmap/RangeBitmapBenchmarkTest.java
@@ -0,0 +1,52 @@
+package com.metamx.collections.bitmap;
+
+import com.metamx.test.annotation.Benchmark;
+import it.uniroma3.mat.extendedset.intset.ConciseSet;
+import it.uniroma3.mat.extendedset.intset.ImmutableConciseSet;
+import org.junit.BeforeClass;
+import org.junit.experimental.categories.Category;
+import org.roaringbitmap.buffer.MutableRoaringBitmap;
+
+import java.util.BitSet;
+
+@Category({Benchmark.class})
+public class RangeBitmapBenchmarkTest extends BitmapBenchmark
+{
+  @BeforeClass
+  public static void prepareRandomRanges() throws Exception
+  {
+    final BitSet expectedUnion = new BitSet();
+    for(int i = 0; i < SIZE; ++i) {
+      ConciseSet c = new ConciseSet();
+      MutableRoaringBitmap r = new MutableRoaringBitmap();
+      {
+        int k = 0;
+        boolean fill = true;
+        while (k < LENGTH) {
+          int runLength =  LENGTH / 100 + rand.nextInt(LENGTH / 100);
+          for (int j = k; fill && j < LENGTH && j < k + runLength; ++j) {
+            c.add(j);
+            r.add(j);
+            expectedUnion.set(j);
+          }
+          k += runLength;
+          fill = !fill;
+        }
+      }
+      minIntersection = LENGTH / 10;
+      for(int k = LENGTH / 2; k < LENGTH / 2 + minIntersection; ++k) {
+        c.add(k);
+        r.add(k);
+        expectedUnion.set(k);
+      }
+      concise[i] = ImmutableConciseSet.newImmutableFromMutable(c);
+      offheapConcise[i] = makeOffheapConcise(concise[i]);
+      roaring[i] = r;
+      immutableRoaring[i] = makeImmutable(r);
+      offheapRoaring[i] = makeOffheap(r);
+      genericConcise[i] = new WrappedImmutableConciseBitmap(offheapConcise[i]);
+      genericRoaring[i] = new WrappedImmutableRoaringBitmap(offheapRoaring[i]);
+    }
+    unionCount = expectedUnion.cardinality();
+  }
+}
---
+++ src/test/java/com/metamx/collections/bitmap/UniformBitmapBenchmarkTest.java
@@ -0,0 +1,49 @@
+package com.metamx.collections.bitmap;
+
+import com.metamx.test.annotation.Benchmark;
+import it.uniroma3.mat.extendedset.intset.ConciseSet;
+import it.uniroma3.mat.extendedset.intset.ImmutableConciseSet;
+import org.junit.BeforeClass;
+import org.junit.experimental.categories.Category;
+import org.roaringbitmap.buffer.MutableRoaringBitmap;
+
+import java.util.BitSet;
+
+@Category({Benchmark.class})
+public class UniformBitmapBenchmarkTest extends BitmapBenchmark
+{
+  @BeforeClass
+  public static void prepareMostlyUniform() throws Exception
+  {
+    final BitSet expectedUnion = new BitSet();
+    final int[] knownTrue = new int[50];
+    for (int i = 0; i < knownTrue.length; ++i) {
+      knownTrue[i] = rand.nextInt(LENGTH);
+    }
+    for(int i = 0; i < SIZE; ++i) {
+      ConciseSet c = new ConciseSet();
+      MutableRoaringBitmap r = new MutableRoaringBitmap();
+      for(int k = 0; k < LENGTH; ++k) {
+        if(rand.nextDouble() < .1) {
+          c.add(k);
+          r.add(k);
+          expectedUnion.set(k);
+        }
+      }
+      for(int k = 0; k < knownTrue.length; ++k) {
+        c.add(knownTrue[k]);
+        r.add(knownTrue[k]);
+        expectedUnion.set(knownTrue[k]);
+      }
+      concise[i] = ImmutableConciseSet.newImmutableFromMutable(c);
+      offheapConcise[i] = makeOffheapConcise(concise[i]);;
+      roaring[i] = r;
+      immutableRoaring[i] = makeImmutable(r);
+      offheapRoaring[i] = makeOffheap(r);
+      genericConcise[i] = new WrappedImmutableConciseBitmap(offheapConcise[i]);
+      genericRoaring[i] = new WrappedImmutableRoaringBitmap(offheapRoaring[i]);
+    }
+    unionCount = expectedUnion.cardinality();
+    minIntersection = knownTrue.length;
+  }
+}
